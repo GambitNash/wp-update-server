5266c8a1c3078b3e19bfb8aff9dd3d0122b3d779
Daniel Wilson <dan.wilson@gambitnash.co.uk>
2015-09-11 12:48:46 +0100

    Add NewRelic Application Monitoring (Set app name) for Gambit Nash use.

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+
+// NewRelic Application Monitoring
+if (extension_loaded('newrelic')) {
+  newrelic_set_appname("GN WP Update Server");
+}
+
+// Get started with wp-update-server
 require __DIR__ . '/loader.php';
 $server = new Wpup_UpdateServer();
 $server->handleRequest();
